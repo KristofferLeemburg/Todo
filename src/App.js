c04653c36b93e21f598974862f485a6d97581f73
Kristoffer Leemburg <kristofferleemburg@gmail.com>
2020-11-12 14:20:47 +0100

    terminal2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Calendar2 from "./ components/calendar"
 
 
 function App() {
   
   return (
     <div>
-      <Calendar />
+      <Calendar2 />
     </div>
     
   );
