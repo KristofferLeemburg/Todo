0d03c4d643822b4da12c99cc88859a80cc49bd09
Kristoffer Leemburg <kristofferleemburg@gmail.com>
2020-11-25 22:19:59 +0100

    Small changes in page title and favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 import Calendar from "./components/calendar"
 import './App.css';
 
 function App() {
   return (
     <div className="main-container">
+      <Helmet>
+                <title>ToDo List</title>
+      </Helmet>
      <Calendar />
     </div> 
   )}
